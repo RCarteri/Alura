d650722b57653be26b78faf3822684331d85927e
Ricardo <ricardo_carteri@hotmail.com>
2021-01-22 20:30:27 -0300

    Validar datas
    Verificando se a data de volta é menor que a de ida
    Verificando se a data de ida é menor do que a data atual

--- python/Django/Formulário/passagens/validation.py
+++ python/Django/Formulário/passagens/validation.py
@@ -7,3 +7,13 @@ def campo_tem_algum_numero(valor_campo, nome_campo, lista_de_erros):
     """Verifica se possui algum digito numérico"""
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = 'Não inclua números neste campo'
+        
+def data_ida_maior_data_volta(data_ida, data_volta, lista_de_erros):
+    """Verifica se a data de volta é menor do que a data de ida"""
+    if  data_volta < data_ida:
+        lista_de_erros['data_volta'] = 'Data de volta não pode ser menor do que a data de ida'
+        
+def data_ida_menor_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """Verifica se a data de ida é menor do que hoje"""
+    if  data_ida < data_pesquisa:
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor do que a data atual'
